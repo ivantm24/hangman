c1567a49b1152694cef7e98395ea2b7c91d2d25c
ivan <ivantm24@gmail.com>
2022-04-21 10:20:29 -0400

    implement make_move

--- hangman/lib/impl/game.ex
+++ hangman/lib/impl/game.ex
@@ -13,6 +13,8 @@ defmodule Hangman.Impl.Game do
    letters: [],
    used: MapSet.new(),
  )
+
+ #######################################
  
  @spec new_game() :: t
  def new_game do
@@ -24,6 +26,25 @@ defmodule Hangman.Impl.Game do
    %__MODULE__{
       letters: word |> String.codepoints
    }
+ end
+
+ ######################################
+
+ @spec make_move(t, String.t) :: { t, Type.tally}
+ def make_move(game = %{ game_state: state }, _guess) 
+ when state in [:won, :lost] do
+     { game, tally(game) }
+ end
+
+
+ defp tally(game) do
+  %{
+    turns_left: game.turns_left,
+    game_state: game.game_state,
+    letters: [],
+    used: game.used |> MapSet.to_list |> Enum.sort
+  }
+
  end
 
 end
