95051edf7561f312c6050b9a886a4500aa6f89e0
ivan <ivantm24@gmail.com>
2022-04-25 16:24:59 -0400

    add remote client

--- text_client/mix.exs
+++ text_client/mix.exs
@@ -14,7 +14,8 @@ defmodule TextClient.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      included_applications: [ :hangman ],  # do not start hangman app when text_client starts
+      extra_applications:    [ :logger  ]
     ]
   end
 
